fa3d650fadaf92a15ef12910585b9298d42a05e3
Charles042 <ekechukwuma20@gmail.com>
2025-02-03 00:11:19 +0100

    Added a push notification for firebase

--- components/nav-bar/_components/slider.tsx
+++ components/nav-bar/_components/slider.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 import Link from "next/link";
 import { House } from "lucide-react";
 import { SliderProps } from "@/app/model/Navbar";
-import NotificationToggle from "./push-notification";
+// import NotificationToggle from "./push-notification";
 
 export const Slider = ({ isOpen,setIsOpen }: SliderProps) => {
   return (
@@ -56,7 +56,7 @@ export const Slider = ({ isOpen,setIsOpen }: SliderProps) => {
           </div>
         </div>
         <div className="flex-center">
-          <NotificationToggle />
+          {/* <NotificationToggle /> */}
         </div>
       </div>
     </motion.div>
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,34 +1,34 @@
-try {
-  importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js");
-  importScripts(
-    "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js"
-  );
+// try {
+//   importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js");
+//   importScripts(
+//     "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js"
+//   );
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBP-jnVFlYK9RjNiCmHVJ3OEDBuRZfaU6Q",
-    authDomain: "crypto-moon-448409-k6.firebaseapp.com",
-    projectId: "crypto-moon-448409-k6",
-    storageBucket: "crypto-moon-448409-k6.firebasestorage.app",
-    messagingSenderId: "699177267701",
-    appId: "1:699177267701:web:48f0febf1d63ca42c430bc",
-    measurementId: "G-SEFWMKPD7F",
-  };
+//   const firebaseConfig = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+//   };
 
-  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
 
-  const messaging = firebase.messaging();
+//   const messaging = firebase.messaging();
 
-  messaging.onBackgroundMessage((payload) => {
-    console.log("Received background message:", payload);
+//   messaging.onBackgroundMessage((payload) => {
+//     console.log("Received background message:", payload);
 
-    const notificationTitle = payload.notification?.title || "New Message";
-    const notificationOptions = {
-      body: payload.notification?.body || "You have a new message",
-      icon: "/icons/icon-192x192.png", // Path to your notification icon
-    };
+//     const notificationTitle = payload.notification?.title || "New Message";
+//     const notificationOptions = {
+//       body: payload.notification?.body || "You have a new message",
+//       icon: "/icons/icon-192x192.png", // Path to your notification icon
+//     };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
-} catch (error) {
-  console.error("Error in firebase-messaging-sw.js:", error);
-}
+//     self.registration.showNotification(notificationTitle, notificationOptions);
+//   });
+// } catch (error) {
+//   console.error("Error in firebase-messaging-sw.js:", error);
+// }
